e6e893342a7cdcd572766ff0263af89c0805b631
Heather Downing <quorralyne@gmail.com>
2025-04-30 19:39:55 +1000

    Added grafana dashboard JSON

--- DeLoreanMonitoring/obj/Debug/net9.0/DeLoreanMonitoring.AssemblyInfo.cs
+++ DeLoreanMonitoring/obj/Debug/net9.0/DeLoreanMonitoring.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeLoreanMonitoring")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+055d5d81588aa6c5c3c07485a12325ca61bdfe8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeLoreanMonitoring")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeLoreanMonitoring")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
